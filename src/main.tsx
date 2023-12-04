1d1470980a7c23e7f0437f87f45175090c0d34d6
sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-12-04 13:25:44 +0100

    removed css from vite

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import MovieView from './MovieView';
 import ErrorPage from './routes/ErrorPage';
+import MovieView from './routes/MovieView';
 
 const router = createBrowserRouter([
   {
