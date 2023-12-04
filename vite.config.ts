dc20d8e61ce639b860dd82c727bfa16ea9553c7e
Hampus Isebring <hampus.isebring@gmail.com>
2023-12-04 11:54:13 +0100

    added test setup + scripts for test & format

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    css: true,
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: vitestConfig.test,
+});
