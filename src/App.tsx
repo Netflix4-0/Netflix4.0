7279960166717afceb4d60158f6e6ec1b41bd6be
Hampus Isebring <hampus.isebring@gmail.com>
2023-12-05 11:06:17 +0100

    Add structure + Swiper.js + placeholder data Co-authored-by: Gabriel Lugo Méndez <gabriel-lugo@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './routes/Header';
+import Home from './routes/Home';
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Outlet />
     </>
   );
