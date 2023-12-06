7cf821b8f71d91ebfaa5629ece36b959df0ab800
Hampus Isebring <hampus.isebring@gmail.com>
2023-12-06 12:11:40 +0100

    add placeholder data + styling to MovieView

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,11 +1,13 @@
-import { Movies } from "../Movies"
-
+import { Heroslide } from '../components/HeroSlide';
+import { Movies } from '../Movies';
+('');
 
 export const HomePage = () => {
   return (
     <>
-    <h1>HomePage</h1>
-<Movies />
+      <Heroslide />
+      <h1>HomePage</h1>
+      <Movies />
     </>
-  )
-}
+  );
+};
--- src/routes/MovieView.tsx
+++ src/routes/MovieView.tsx
@@ -1,9 +1,57 @@
-const MovieView = () => {
+import './MovieView.css';
+
+export const MovieView = () => {
+  const movie = {
+    title: 'The Lord of the Rings: The Two Towers',
+    year: 2002,
+    rating: 'PG-13',
+    actors: ['Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'],
+    genre: 'Action, Adventure, Drama',
+    synopsis:
+      "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard.",
+    thumbnail:
+      'https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UX380_CR0,14,380,562_.jpg',
+    poster: 'https://wallpapercave.com/wp/wp3871647.jpg',
+  };
+
+  const genres = movie.genre.split(', ');
+
   return (
     <>
-      <h1>Movie asd as s</h1>
+      <div className='movie-view-container'>
+        <div
+          className='movie-poster'
+          style={{
+            backgroundImage: `url(${movie.poster})`,
+          }}
+        />
+        <div className='movie-details'>
+          <h2>{movie.title}</h2>
+          <div className='genres-list'>
+            <div className='genre-container'>
+              {genres.map((genre, index) => (
+                <div key={index} className='genre-item'>
+                  {genre}
+                </div>
+              ))}
+            </div>
+          </div>
+          <p>Released in: {movie.year}</p>
+          <p>Rating: {movie.rating}</p>
+          <p>{movie.synopsis}</p>
+          <div className='actors-list'>
+            <h3>Actors:</h3>
+            <ul>
+              {movie.actors.map((actor, index) => (
+                <li key={index}>{actor}</li>
+              ))}
+            </ul>
+          </div>
+          <div className='movie-thumbnail'>
+            <img src={movie.thumbnail} alt={movie.title} />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
-
-export default MovieView;
