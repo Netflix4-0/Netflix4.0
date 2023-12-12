d7ab1e5bdda19d7cdf18c86ec7e1725555b85a67
Hampus Isebring <hampus.isebring@gmail.com>
2023-12-12 16:01:09 +0100

    Added illustration to error page + styling Co-authored-by: Gabriel Lugo Méndez <gabriel-lugo@users.noreply.github.com>

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
+import './ErrorPage.css';
+
 const ErrorPage = () => {
   return (
-    <>
-      <h1 style={{ color: 'red' }}>Big Error!</h1>
-    </>
+    <div className='error-container'>
+      <img src='https://github.com/Netflix4-0/Netflix4.0/assets/116897452/c7020dbf-eea9-4fa3-aedb-38a6238f5da9' />
+      <h1>Page not found!</h1>
+      <Link to={'/'}>
+        <button className='error-page-button'>To Homepage</button>
+      </Link>
+    </div>
   );
 };
 
